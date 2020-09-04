d99350927229f25aae0ca962dd215b46d03f5721
Shreyas Ramaswamy <shrer@microsoft.com>
2020-09-04 23:41:26 +0530

    Initial commit

---
+++ MstestDotnetCoreTests/UnitTest1.cs
@@ -0,0 +1,32 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MstestDotnetCoreTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+
+        }
+    }
+}
